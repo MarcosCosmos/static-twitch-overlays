1912ea6a9671f7ce8704577e0bab9e85dec52954
Unknown <m.cosmos42@gmail.com>
2022-01-26 11:56:59 +1100

    Fixing issues with new embed.html/.js logic
    Also, generalised embed.js to be applicable to both embed.html and to se embedding.

--- core/embed.js
+++ core/embed.js
@@ -1,48 +1,56 @@
 import CompositeModule from './CompositeModule.js';
-export default async (config, start=true) => {
+export default async (config, start=true, baseUrl='https://marcoscosmos.gitlab.io/static-twitch-overlays') => {
+    let themeName = config.widget.theme;
+
+    let themeJsUrl = `${baseUrl}/themes/${themeName}/displays.js`;
+    if((await fetch(themeJsUrl)).status != 200){
+        //fallback to the default theme
+        themeName = 'default';
+        themeJsUrl = `${baseUrl}/themes/${themeName}/displays.js`;
+    }
+    
+    //only changes the css on a successful theme being grabbed
+    
+    let oldStyleSheet = document.querySelector('#themeStyle');
+    if(oldStyleSheet) {
+        oldStyleSheet.remove();
+    }
+    let newStyleSheet = document.createElement('link');
+    newStyleSheet.rel = 'stylesheet';
+    newStyleSheet.href = `${baseUrl}/themes/${themeName}/style.css`;
+    newStyleSheet.id = 'themeStyle';
+    
+    document.head.appendChild(newStyleSheet);
+
     let theModule = new CompositeModule(config);
     await theModule.finalizeBoxes();
     switch(config.boxToShow) {
-        case 'display':
-            let work = async () => {
-                let themeName = config.widget.theme;
-
-                let themeJsUrl = `https://marcoscosmos.gitlab.io/static-twitch-overlays/themes/${themeName}/displays.js`;
-                if((await fetch(themeJsUrl)).status != 200){
-                    //fallback to the default theme
-                    themeName = 'default';
-                    themeJsUrl = `/themes/${themeName}/displays.js`;
+        case 'display': {
+            let displayBoxes = (await import(themeJsUrl)).default;
+            let displayBox = await (displayBoxes[config.widget.moduleTypeName].bind(theModule.widget))();
+            const app = Vue.createApp({
+                components: {
+                    display: displayBox
+                },
+                template: `<display></display>`
+            });
+            const vm = app.mount('#embed');
+            if(start) {
+                theModule.start();
+            }
+            break;
+        }
+        default: {
+            if(config.boxToShow == 'controls') {
+                theModule.isInManualMode = true;
+                theModule.widget.isInManualMode = true;
+                for(let each of Object.values(theModule.services)) {
+                    each.isInManualMode = true;
                 }
-                let displayBoxes = (await import(themeJsUrl)).default;
-                let displayBox = (displayBoxes[config.widgetType].bind(activeWidget))();
-                
-                //only changes the css on a successful theme being grabbed
-                
-                let oldStyleSheet = document.querySelector('#themeStyle');
-                if(oldStyleSheet) {
-                    oldStyleSheet.remove();
-                }
-                let newStyleSheet = document.createElement('link');
-                newStyleSheet.rel = 'stylesheet';
-                newStyleSheet.href = `https://marcoscosmos.gitlab.io/static-twitch-overlays/themes/${themeName}/style.css`;
-                newStyleSheet.id = 'themeStyle';
-                
-                document.head.appendChild(newStyleSheet);
-
-                const app = Vue.createApp({
-                    components: {
-                        display: displayBox
-                    },
-                    template: `<display></display>`
-                });
-                const vm = app.mount('#embed');
-                if(start) {
-                    theModule.start();
+                for(let each of Object.values(theModule.handlers)) {
+                    each.isInManualMode = true;
                 }
             }
-            work();
-            break;
-        default:
             const app = Vue.createApp({
                 data() {
                     return {module: theModule};
@@ -62,7 +70,7 @@ export default async (config, start=true) => {
                     </div>
                 `,
             });
-            const vm = app.mount('#embed'); 
+            const vm = app.mount('#embed');
+        } 
     }
 };
-doWork();
